745bda3189c7384d4b61decea4e4a6ef6d84323d
сулпак <sm.mederovich@gmail.com>
2022-07-19 17:21:17 +0600

    HWW

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -22,39 +22,23 @@ urlpatterns = [
     path('', views.homepage),
     path('PC/', views.PC),
     path('categories/', views.categories),
+    path('about/', views.AboutView.as_view()),
+    path('hypertext/', views.HyperText.as_view()),
+    path('Web_List/', views.Web_List.as_view()),
+    path('Browser/', views.Browser.as_view()),
+    path('HTTP/', views.HTTP.as_view()),
+    path('HTTPS/', views.HTTPS.as_view())
 ]
+# ]
 
-
-
-
-
-
-
-
-
-# """geekshop URL Configuration
-# The `urlpatterns` list routes URLs to views. For more information please see:
-#     https://docs.djangoproject.com/en/4.0/topics/http/urls/
-# Examples:
-# Function views
-#     1. Add an import:  from my_app import views
-#     2. Add a URL to urlpatterns:  path('', views.home, name='home')
-# Class-based views
-#     1. Add an import:  from other_app.views import Home
-#     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-# Including another URLconf
-#     1. Import the include() function: from django.urls import include, path
-#     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-# """
-#
 # from django.contrib import admin
 # from django.urls import path
-# from product.views import \
-#     homepage, PC, ExampleView
+# from product import views
 #
 # urlpatterns = [
 #     path('admin/', admin.site.urls),
-#     path('', homepage),
-#     path('PC/', PC),
-#     path('categories/', ExampleView.as_view),
+#     path('', views.homepage),
+#     path('PC/', views.PC),
+#     path('categories/', views.categories),
+#     path('example/', views.AboutView.as_view())
 # ]
--- product/views.py
+++ product/views.py
@@ -5,7 +5,27 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 class AboutView(TemplateView):
-    template_name = "example.html"
+    template_name = "about.html"
+
+
+class HyperText(TemplateView):
+    template_name = "hypertext.html"
+
+
+class Web_List(TemplateView):
+    template_name = "Web_List.html"
+
+
+class Browser(TemplateView):
+    template_name = "Browser.html"
+
+
+class HTTP(TemplateView):
+    template_name = "HTTP.html"
+
+
+class HTTPS(TemplateView):
+    template_name = "HTTPS.html"
 
 
 def homepage(request):
@@ -22,6 +42,71 @@ def categories(request):
     products = PC.objects.all()
     data = {"all_laptops": products}
     return render(request, "categories.html", context=data)
+
+# def categories_view(request):
+#     return HttpResponse("<h1>Список Категорий")
+
+# from django.shortcuts import render, HttpResponse
+# from django.views.generic import TemplateView
+# from .models import PC, Categories
+#
+#
+# class AboutView(TemplateView):
+#     template_name = "about.html"
+#
+#
+# def about(request):
+#     return render(request, 'about.html')
+#
+#
+# # Create your views here.
+# def homepage(request):
+#     # SELECT * FROM Vegetables
+#     products = PC.objects.all()  # list
+#     context = {"all_vegetables": products}
+#     return render(request, "product_list.html", context)
+#
+#
+# def personal_computer(request):
+#     laptop_object = PC.objects.get(id=1)
+#     description = f"{laptop_object.name}, {laptop_object.description}"
+#     return HttpResponse(description)
+#
+#
+# def categories(request):
+#     products = Categories.objects.all()
+#     data = {"all_laptops": products}
+#     return render(request, "categories.html", context=data)
+
+
+# def categories_view(request):
+#     return HttpResponse("<h1>Список Категорий")
+
+
+# from django.shortcuts import render, HttpResponse
+# from .models import PC
+# from django.views.generic import TemplateView
+#
+#
+# # Create your views here.
+# class AboutView(TemplateView):
+#     template_name = "exampld.html"
+#
+#
+# def homepage(request):
+#     return render(request, "index.html")
+#
+#
+# def personal_computer(request):
+#     laptop_object = PC.objects.get(id=1)
+#     description = f"{laptop_object.name}, {laptop_object.description}"
+#     return HttpResponse(description)
+#
+#
+# def categories(request):
+#     products = PC.objects.all()
+#     data = {"all_laptops": products}
+#     return render(request, "categories.html", context=data)
 # def categories_view(request):
 #     return HttpResponse("<h1>Список Категорий")
 
@@ -59,3 +144,5 @@ def categories(request):
 # # def categories_view(request):
 # #     return HttpResponse("<h1>Список Категорий")
 
+# def example():
+#     return None
